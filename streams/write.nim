0ea1e9c031c67b3cdf5b0885852a48202bd5ec6c
SiriusLee69 <n1ghty@t1mx4.com>
2025-12-07 00:58:16 +0100

    something

---
+++ streams/write.nim
@@ -0,0 +1,23 @@
+import std/unittest
+
+proc toByteSeq(x: uint64): seq[uint8] =
+    if x < 255'u64:
+        result.add( cast[uint8](x) )
+    elif x < 65535'u64: #max uint16 size; also: multiplications are done at compile time (wrote them for better readability)
+        result.add( cast[uint8](x shr 8*0))
+        result.add( cast[uint8](x shr 8*1))
+        result.add( cast[uint8](x shr 8*2))
+        result.add( cast[uint8](x shr 8*3))
+    elif x < 4294967295'u64: #max uint32 size
+        result.add( cast[uint8](x) )
+        result.add( cast[uint8](x shr 8*1))
+        result.add( cast[uint8](x shr 8*2))
+        result.add( cast[uint8](x shr 8*3))
+        result.add( cast[uint8](x shr 8*4))
+        result.add( cast[uint8](x shr 8*5))
+        result.add( cast[uint8](x shr 8*6))
+        result.add( cast[uint8](x shr 8*7))
+
+echo $toByteSeq(23'u64)
+echo $toByteSeq(235135531'u64)
+
