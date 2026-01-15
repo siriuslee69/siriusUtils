f310856a8b84fa428e6597a965fbea59f9135feb
SiriusLee69 <n1ghty@t1mx4.com>
2026-01-15 20:43:19 +0100

    eh

--- src/math/genetic/genetic.nim
+++ src/math/genetic/genetic.nim
@@ -4,18 +4,23 @@ type
         w: int    #weight
         p: proc(x, y: T): Q 
 
+
+proc add[T, Q: SomeNumber](s: seq[Op], op: proc(x,y:T):Q, name: string, weight: int ): void =
+    s.add((name, weight, op))
 const 
     ops*: array[8, Op[int, int]] = [
         ( "and", 1, proc(x,y: int): int = x and y),
         ( "xor", 1, proc(x,y: int): int = x xor y),
         ( "or", 1, proc(x,y: int): int = x or y),
         ( "not", 1, proc(x,y: int): int = not x),
-        ( "sub", 1, proc(x,y:int): int = x - y ),
-        ("add", 1, proc(x,y: int): int = x + y),
-        ( "mul", 1, proc(x,y:int): int = x * y),
-        ( "div", 1, proc(x,y:int): int = 
-            if y == 0: return x
-            else: return (x / y).toInt())
+        ( "sub", 1, proc(x,y: int): int = x - y ),
+        ( "add", 1, proc(x,y: int): int = x + y),
+        ( "mul", 1, proc(x,y: int): int = x * y),
+        ( "div", 1, proc(x,y: int): int = 
+            if y == 0: 
+                return x
+            else: 
+                return (x / y).toInt())
         ]
 
 when defined(test):
